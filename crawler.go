b45f049aa24443918031053981bd66bf7721d226
sunliang <sunliang711@163.com>
2017-02-22 15:00:31 +0800

    add GetLinks func

--- crawler.go
+++ crawler.go
@@ -2,9 +2,53 @@ package httputil
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
+	"time"
 )
+
+var ReLink = regexp.MustCompile(`<\s*a\s+href\s*=\s*"([^"]+)"`)
+
+//根据给定的url返回url中的所有链接
+//@param url string
+//@return ([]string,error)
+func GetLinks(url string) ([]string, error) {
+	if len(url) == 0 {
+		return nil, errors.New("The given url is empty string")
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	//get links from body
+	links := ReLink.FindAllStringSubmatch(string(body), -1)
+	if len(links) == 0 {
+		return nil, fmt.Errorf("get no link")
+	}
+	var retLinks []string
+	//v是匹配的链接的字符串<h href="xxx" 以及链接xxx组成的slice
+	for _, v := range links {
+		if len(v) == 2 {
+			fmt.Printf("get link: %s\n", v[1])
+			retLinks = append(retLinks, v[1])
+		}
+	}
+	return retLinks, nil
+}
 
 //根据http.Get的返回值(类型为*http.Response)来判断请求的URL是否是html页面
 //@param res *http.Response
